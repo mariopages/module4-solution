f72d1ab5140859fe74ac445b3765f9b6b92b7e87
Mario <mario_pages@yahoo.com>
2017-04-22 14:07:43 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+(function () {
+  var names =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
+for ( var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0)
+  firstLetter = firstLetter.toLowerCase();
+   if (firstLetter == "j") {
+      byeSpeaker(names[i]);
+    } else {
+     helloSpeaker(names[i]);
+    }
+}
+})();
